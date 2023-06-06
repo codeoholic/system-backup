be7af2867c90dc0fe3eaf6c586eb788f60dffb67
Mohit <mohit@hey.com>
2023-06-06 12:21:02 +0530

    x

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 . env.sh
 
 # PRINT PATHS TO BE TRAVERSED
-echo ${path}
+echo ${path[*]}
 
 # PRINT LENGTH OF ARRAY
 echo ${#path[@]}
@@ -12,6 +12,16 @@ echo ${path[0]}
 
 for d in ${path[0]}/*; do
     if [ -d "$d" ]; then
-        echo "$d"
+        echo "path is $d"
+        for projects in $d/*; do
+            if [ -d "$projects" ]; then
+                echo "project is $projects"
+                for inner_folder in $projects/*; do
+                    if [ -d "$inner_folder" ]; then
+                        echo "inner folder is $inner_folder"
+                    fi
+                done
+            fi
+        done
     fi
 done
